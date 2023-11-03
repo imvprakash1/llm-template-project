cfe15cabbe0f8fd91c27ae91f7c3600943c22c52
Vaibhav Prakash <imvprakash1@gmail.com>
2023-11-03 09:12:37 +0530

    Initializing project

---
+++ widget-section/src/components/Home.vue
@@ -0,0 +1,8 @@
+<script setup>
+import WidgetSection from './WidgetSection.vue'
+</script>
+<template>
+  <div class="container">
+    <WidgetSection />
+  </div>
+</template>
---
+++ widget-section/src/components/WidgetItem.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  props: ['widget']
+}
+</script>
+<template>
+  <div class="widget-item" draggable>
+    {{ widget.name }}
+  </div>
+</template>

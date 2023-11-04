103a43fb89c8ea45371c03aedee570fed6e22543
Vaibhav Prakash <imvprakash1@gmail.com>
2023-11-04 22:47:23 +0530

    Added Testing prompt UI

--- widget-section/src/components/TestingPrompt.vue
+++ widget-section/src/components/TestingPrompt.vue
@@ -1,13 +1,81 @@
 <template>
   <Navbar />
-  <div>Testing</div>
+  <div class="container">
+    <div>
+      <h3>{{ template }}</h3>
+    </div>
+    <form v-on:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="language">Language</label>
+        <input
+          type="text"
+          class="form-control"
+          id="language"
+          aria-describedby="emailHelp"
+          placeholder="Language, i.e. Python, Java, JavaScript etc."
+          v-model="language"
+        />
+      </div>
+      <div class="form-group">
+        <label for="code">Code</label>
+        <input
+          type="text"
+          class="form-control"
+          id="code"
+          placeholder="Please paste your code here"
+          v-model="code"
+        />
+      </div>
+      <div class="form-group">
+        <label for="tone">Tone</label>
+        <input type="text" class="form-control" id="tone" placeholder="Tone" v-model="tone" />
+      </div>
+      <button class="btn btn-primary my-2">Submit</button>
+    </form>
+    <div class="form-control response" id="response">Your response will appear here.</div>
+  </div>
 </template>
 <script>
 import Navbar from './Navbar.vue'
+import { defineComponent, ref } from 'vue'
+import axios from 'axios'
 
-export default {
+let language = ref('')
+let code = ref('')
+let tone = ref('')
+let response = ref(null)
+export default defineComponent({
   components: {
     Navbar
+  },
+  async setup() {
+    const data = await axios.get(`${import.meta.env.VITE_BACKENDAPI}/prompt/testing`)
+    return {
+      template: data.data,
+      language,
+      code,
+      tone,
+      response
+    }
+  },
+  methods: {
+    async onSubmit(e) {
+      e.preventDefault()
+      try {
+        document.getElementById('response').innerText = 'Loading...'
+        const data = await axios.post(`${import.meta.env.VITE_BACKENDAPI}/prompt/testing`, {
+          language: language.value,
+          code: code.value,
+          tone: tone.value,
+          inputVariables: ['language', 'code']
+        })
+        response = data.data
+        console.log(response)
+        document.getElementById('response').innerText = response
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
   }
-}
+})
 </script>
--- widget-section/src/components/TonePrompt.vue
+++ widget-section/src/components/TonePrompt.vue
@@ -1,13 +1,74 @@
 <template>
   <Navbar />
-  <div>Tone</div>
+  <div class="container">
+    <div>
+      <h3>{{ template }}</h3>
+    </div>
+    <form v-on:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="tone">Tone</label>
+        <input
+          type="text"
+          class="form-control"
+          id="tone"
+          aria-describedby="emailHelp"
+          placeholder="Please enter tone."
+          v-model="tone"
+        />
+      </div>
+      <div class="form-group">
+        <label for="context">Context</label>
+        <input
+          type="text"
+          class="form-control"
+          id="context"
+          placeholder="Please enter context here"
+          v-model="context"
+        />
+      </div>
+      <button class="btn btn-primary my-2">Submit</button>
+    </form>
+    <div class="form-control response" id="response">Your response will appear here.</div>
+  </div>
 </template>
 <script>
 import Navbar from './Navbar.vue'
+import { defineComponent, ref } from 'vue'
+import axios from 'axios'
 
-export default {
+let tone = ref('')
+let context = ref('')
+let response = ref(null)
+export default defineComponent({
   components: {
     Navbar
+  },
+  async setup() {
+    const data = await axios.get(`${import.meta.env.VITE_BACKENDAPI}/prompt/tone`)
+    return {
+      template: data.data,
+      tone,
+      context,
+      response
+    }
+  },
+  methods: {
+    async onSubmit(e) {
+      e.preventDefault()
+      try {
+        document.getElementById('response').innerText = 'Loading...'
+        const data = await axios.post(`${import.meta.env.VITE_BACKENDAPI}/prompt/tone`, {
+          tone: tone.value,
+          context: context.value,
+          inputVariables: ['tone', 'context']
+        })
+        response = data.data
+        console.log(response)
+        document.getElementById('response').innerText = response
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
   }
-}
+})
 </script>
